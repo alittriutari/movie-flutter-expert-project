e17b7c2183c69bb426a06c6f46ac6a52a7f233ee
alittriutari <alittriutari@gmail.com>
2022-06-12 21:33:56 +0800

    fix error in injection.dart

--- lib/injection.dart
+++ lib/injection.dart
@@ -12,15 +12,15 @@ import 'package:core/domain/usecases/get_watchlist_movies.dart';
 import 'package:core/domain/usecases/get_watchlist_status.dart';
 import 'package:core/domain/usecases/remove_watchlist.dart';
 import 'package:core/domain/usecases/save_watchlist.dart';
-import 'package:core/domain/usecases/search_movies.dart';
 import 'package:core/presentation/provider/movie_detail_notifier.dart';
 import 'package:core/presentation/provider/movie_list_notifier.dart';
-import 'package:core/presentation/provider/movie_search_notifier.dart';
 import 'package:core/presentation/provider/popular_movies_notifier.dart';
 import 'package:core/presentation/provider/top_rated_movies_notifier.dart';
 import 'package:core/presentation/provider/watchlist_movie_notifier.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
+import 'package:search/domain/usecases/search_movies.dart';
+import 'package:search/presentation/provider/movie_search_notifier.dart';
 
 final locator = GetIt.instance;
 
